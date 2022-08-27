4824c06a27e63c121dcd5d00713d1bff7efd364f
luciano-vaimberg <110951633+luciano-vaimberg@users.noreply.github.com>
2022-08-27 11:22:09 -0300

    clases creadas

---
+++ AppVelez/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+class SociosSemiPlenos(models.Model):
+    nombre=models.CharField(max_length=25)
+    apellido=models.CharField(max_length=25)
+    genero=models.CharField(max_length=25)
+    edad=models.IntegerField()
+    documento=models.IntegerField()
+    email=models.EmailField()
+    direccion=models.CharField(max_length=50)
+    localidad=models.CharField(max_length=50)
+    provincia=models.CharField(max_length=50)
+    pais=models.CharField(max_length=50)
+    numero_socio=models.IntegerField()
+    
+class SociosPlenos(models.Model):
+    nombre=models.CharField(max_length=25)
+    apellido=models.CharField(max_length=25)
+    genero=models.CharField(max_length=25)
+    edad=models.IntegerField()
+    documento=models.IntegerField()
+    email=models.EmailField()
+    direccion=models.CharField(max_length=50)
+    localidad=models.CharField(max_length=50)
+    provincia=models.CharField(max_length=50)
+    pais=models.CharField(max_length=50)
+    numero_socio=models.IntegerField()
+
+class Empleados(models.Model):
+    nombre=models.CharField(max_length=25)
+    apellido=models.CharField(max_length=25)
+    genero=models.CharField(max_length=25)
+    edad=models.IntegerField()
+    documento=models.IntegerField()
+    email=models.EmailField()
+    direccion=models.CharField(max_length=50)
+    localidad=models.CharField(max_length=50)
+    provincia=models.CharField(max_length=50)
+    pais=models.CharField(max_length=50)
+    cargo=models.CharField(max_length=50)
+    sueldo=models.IntegerField()
+
+    
